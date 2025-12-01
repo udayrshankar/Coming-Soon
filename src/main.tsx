bacd3b211946f92e35d0152c2e639e90e439cabf
udayrshankar <138763436+udayrshankar@users.noreply.github.com>
2025-12-01 15:38:38 +0530

    Website analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+// 1. Import PostHog
+import posthog from 'posthog-js'
+
+// 2. Initialize it (Replace with your Key from PostHog dashboard)
+posthog.init('phc_uU45IWF2MvjZhx3f9CmHD847RAtqkErnq4MqUliigAY', {
+    api_host: 'https://us.i.posthog.com', // or 'https://eu.i.posthog.com'
+    person_profiles: 'always', // Recommended for anonymous users
+})
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
